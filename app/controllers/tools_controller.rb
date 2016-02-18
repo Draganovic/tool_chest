f0ad28a1c5e4512467c19583b64dc20ebd882daa
Admir Draganovic <admir.draganovic@gmail.com>
2016-02-18 11:34:22 -0700

    For reference

---
+++ app/controllers/tools_controller.rb
@@ -0,0 +1,53 @@
+class ToolsController < ApplicationController
+
+  def index
+    @tools = Tool.all
+  end
+
+  def show
+    @tool = Tool.find(params[:id])
+  end
+
+  def new
+    @tool = Tool.new
+  end
+
+  def edit
+    @tool = Tool.find(params[:id])
+  end
+
+  def update
+    @tool = Tool.find(params[:id])
+    if @tool.update(tool_params)
+      redirect_to tool_path(@tool.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @tool = Tool.find(params[:id])
+    @tool.destroy
+    redirect_to tools_path
+  end
+
+  def create
+    #generat new tool
+    @tool = Tool.new(tool_params)
+    #add attributes to that tool
+    #try to save tool
+    if @tool.save
+      #if tool saves send user to view tool(database will send user)
+      redirect_to tool_path(@tool.id)
+    else
+      #if not saved we will render the new view
+      render :new
+    end
+  end
+
+  private
+
+  def tool_params
+    params.require(:tool).permit(:name, :quantity, :price)
+  end
+end
